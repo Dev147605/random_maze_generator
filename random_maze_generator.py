d72746b1847ddc217bd19bc50d94519643d24482
Dev147605 <deveshdpatel108@gmail.com>
2026-03-31 20:57:33 +0100

    initalized the maze

---
+++ random_maze_generator.py
@@ -0,0 +1,18 @@
+import numpy as np
+import math
+import panel as pn
+
+numberSquares = 49
+maze = np.ones((int(math.sqrt(numberSquares)),int(math.sqrt(numberSquares))),dtype = "int32")
+
+#region initalising maze
+def initalize_maze():
+    maze[int(math.sqrt(numberSquares)-1),int(math.sqrt(numberSquares)-2)] = 99
+    maze[int(math.sqrt(numberSquares)-2),int(math.sqrt(numberSquares)-2)] = 99
+    maze[0,1] = 99
+    maze[1,1] = 99
+    
+    print(maze)
+#endregion
+
+initalize_maze()
