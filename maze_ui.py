9c89eecc49d776f253876f73cd041469e8b71c44
Dev147605 <deveshdpatel108@gmail.com>
2026-03-31 21:08:18 +0100

    initialized the panel for the ui script

---
+++ maze_ui.py
@@ -0,0 +1,20 @@
+import panel as pn
+import math
+pn.extension()
+
+select = pn.widget.Select(name = "Select",options={"25":25,
+                                                   "36":36,
+                                                   "49":49,
+                                                   "64":64,
+                                                   "81":82,
+                                                   "100":100})
+button = pn.widgets.Button(name="Generate Maze")
+def on_click(event):
+    print(f"Generating a maze with size {int(math.sqrt(select.value))} x {int(math.sqrt(select.value))}")
+
+button.on_click(on_click)
+
+app = pn.Column(select,button)
+
+app.servable()
+pn.serve({"maze generator": app})
