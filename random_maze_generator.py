cd9e52a134c7b5f432b9e8a601f51c980c01720d
Dev147605 <deveshdpatel108@gmail.com>
2026-06-18 18:20:12 +0100

    added the maze generation function

--- random_maze_generator.py
+++ random_maze_generator.py
@@ -1,40 +1,72 @@
 import numpy as np
 import math
 import maze_ui
+import random
 
 numberSquares = maze_ui.numberSquare
 maze = np.ones((int(math.sqrt(numberSquares)),int(math.sqrt(numberSquares))),dtype = "int32")
-valid_maze = np.zeros((int(math.sqrt(numberSquares))-2,int(math.sqrt(numberSquares))-2),dtype = "int32")
+
+#creates inner maze
+valid_maze = np.ones((int(math.sqrt(numberSquares))-2,int(math.sqrt(numberSquares))-2),dtype = "int32")
 
 #region initalising maze
-def initalize_maze(maze,valid_maze):
-    maze[1:int(math.sqrt(numberSquares)-1),1:int(math.sqrt(numberSquares)-1)] = valid_maze
+def initalize_maze(maze):
+    
+    #initialises the 99 values in the bottom right corner
     maze[int(math.sqrt(numberSquares)-1),int(math.sqrt(numberSquares)-2)] = 99
-    maze[int(math.sqrt(numberSquares)-2),int(math.sqrt(numberSquares)-2)] = 99
+    
+    #initialises the 99 values on the top left corner
     maze[0,1] = 99
-    maze[1,1] = 99
+    
+    inner_maze = generate_inner_maze()
+    maze[1:int(math.sqrt(numberSquares)) - 1,1:int(math.sqrt(numberSquares)) - 1] = inner_maze
     print(maze)
-    generating_maze(valid_maze)
+
+
 #endregion
+
 
 #region generating maze
-def generating_maze(valid_maze):
+def generate_inner_maze():
+    valid_maze[int(math.sqrt(numberSquares)-3),int(math.sqrt(numberSquares)-3)] = 99
+    valid_maze[0,0] = 99
+    #print(valid_maze)
     starting_node = (0,0)
-    target_node = (len(valid_maze)-1,len(valid_maze)-1)
-    (current_node) = starting_node
-    stack = [starting_node]
-    visited = []
-    while stack:
-        #right = current_node
-        #print(current_node, type(current_node))
-        (row,col) = current_node
-        neighbours = [(row+1,col),
-                        (row-1,col),
-                        (row,col+1),
-                        (row,col-1)]
-        #right left down up
-    print(neighbours)
+    target_node = (int(math.sqrt(numberSquares)-3),int(math.sqrt(numberSquares)-3))
+    current_pos = starting_node
+
+    stack = [current_pos]
+    visited = set()
+
+    max_column_row = int(math.sqrt(numberSquares)-3)
+    min_column_row = 0
+
+    while current_pos != target_node:
+
+        (row,col) = current_pos
+        #neighbours are in the order of up, down, left, right
+        neighbours_changes = [(-1,0),(1,0),(0,1),(-1,0)]
+        valid_neighbours = []
+
+        for i,j in neighbours_changes:
+            new_row = row + i
+            new_col = col + j
+            if (new_row <= max_column_row and new_row >= min_column_row) and (new_col <= max_column_row and new_col >= min_column_row):
+                new_pos = (new_row,new_col)
+                if new_pos not in visited:
+                    valid_neighbours.append(new_pos)
+
+        #using random to work out the next "node" in the path using the random module
+        next_position = random.choice(valid_neighbours)
+
+        #adds the current pos to the visited set
+        visited.add(current_pos)
+        stack.append(current_pos)
+        #changes the position of the current pos to the next pos
+        current_pos = next_position
+        row,col = current_pos
+        valid_maze[row,col] = 99
+    return valid_maze
 #endregion
 
-
-initalize_maze(maze,valid_maze)
+initalize_maze(maze)
