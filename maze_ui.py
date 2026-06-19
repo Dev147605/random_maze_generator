3a6b90a480a5a9bfebf947901e2d9fe6d137c55a
Dev147605 <deveshdpatel108@gmail.com>
2026-06-19 16:19:40 +0100

    added the maze to the panel screen

--- maze_ui.py
+++ maze_ui.py
@@ -1,22 +1,30 @@
 import panel as pn
 import math
-pn.extension()
+import random_maze_generator
+import matplotlib.pyplot as plt
+pn.extension('matplotlib')
 
-select = pn.widgets.Select(name = "Select",options={"25":25,
-                                                   "49":49,
-                                                   "81":81,
-                                                   "121":121,
-                                                   "169":169})
+select = pn.widgets.Select(name = "Select",options={"5 x 5":25,
+                                                   "7 x 7":49,
+                                                   "9 x 9":81,
+                                                   "11 x 11":121,
+                                                   "13 x 13":169})
 button = pn.widgets.Button(name="Generate Maze")
-
+maze_display = pn.pane.Matplotlib(width=500, height=500, tight=True)
 def on_click(event):
     global numberSquare
     numberSquare = select.value
     print(f"Generating a maze with dimensions {int(math.sqrt(select.value))} x {int(math.sqrt(select.value))}")
-
+    fig = random_maze_generator.initalize_maze(numberSquare)
+    maze_display.object = fig
+    maze_display.param.trigger('object')
+    plt.close(fig)
 button.on_click(on_click)
 
-app = pn.Column(select,button)
+app = pn.Column(pn.pane.Markdown("## Maze Generator"),
+                select,
+                button,
+                maze_display)
 
 app.servable()
 pn.serve(app,show=True)
--- random_maze_generator.py
+++ random_maze_generator.py
@@ -1,35 +1,40 @@
+#import maze_ui
 import numpy as np
 import math
-import maze_ui
 import random
+import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
-numberSquares = maze_ui.numberSquare
-maze = np.ones((int(math.sqrt(numberSquares)),int(math.sqrt(numberSquares))),dtype = "int32")
-
-#creates inner maze
-valid_maze = np.ones((int(math.sqrt(numberSquares))-2,int(math.sqrt(numberSquares))-2),dtype = "int32")
 
 #region initalising maze
-def initalize_maze(maze):
-    
+def initalize_maze(numberSquares):
+    maze = np.zeros((int(math.sqrt(numberSquares)),int(math.sqrt(numberSquares))),dtype = "int32")
     #initialises the 99 values in the bottom right corner
-    maze[int(math.sqrt(numberSquares)-1),int(math.sqrt(numberSquares)-2)] = 99
+    maze[int(math.sqrt(numberSquares)-1),int(math.sqrt(numberSquares)-2)] = 1
     
     #initialises the 99 values on the top left corner
-    maze[0,1] = 99
+    maze[0,1] = 1
     
-    inner_maze = generate_inner_maze()
+    inner_maze = generate_inner_maze(numberSquares)
     maze[1:int(math.sqrt(numberSquares)) - 1,1:int(math.sqrt(numberSquares)) - 1] = inner_maze
-    print(maze)
+    #print(maze)
+    #generate_maze_object()
 
+    fig, ax = plt.subplots(figsize=(5, 5))
+    cmap = mcolors.ListedColormap(["black", "white"])
+    ax.imshow(maze, cmap=cmap, vmin=0, vmax=1)
+    ax.axis("off")
+    plt.tight_layout()
+
+    return fig
 
 #endregion
 
-
 #region generating maze
-def generate_inner_maze():
-    valid_maze[int(math.sqrt(numberSquares)-3),int(math.sqrt(numberSquares)-3)] = 99
-    valid_maze[0,0] = 99
+def generate_inner_maze(numberSquares):
+    valid_maze = np.zeros((int(math.sqrt(numberSquares))-2,int(math.sqrt(numberSquares))-2),dtype = "int32")
+    valid_maze[int(math.sqrt(numberSquares)-3),int(math.sqrt(numberSquares)-3)] = 1
+    valid_maze[0,0] = 1
     #print(valid_maze)
     starting_node = (0,0)
     target_node = (int(math.sqrt(numberSquares)-3),int(math.sqrt(numberSquares)-3))
@@ -45,7 +50,7 @@ def generate_inner_maze():
 
         (row,col) = current_pos
         #neighbours are in the order of up, down, left, right
-        neighbours_changes = [(-1,0),(1,0),(0,1),(-1,0)]
+        neighbours_changes = [(-1,0),(1,0),(0,1),(0,-1)]
         valid_neighbours = []
 
         for i,j in neighbours_changes:
@@ -57,16 +62,23 @@ def generate_inner_maze():
                     valid_neighbours.append(new_pos)
 
         #using random to work out the next "node" in the path using the random module
-        next_position = random.choice(valid_neighbours)
+        if len(valid_neighbours)!=0:
+            next_position = random.choice(valid_neighbours)
 
-        #adds the current pos to the visited set
-        visited.add(current_pos)
-        stack.append(current_pos)
-        #changes the position of the current pos to the next pos
-        current_pos = next_position
-        row,col = current_pos
-        valid_maze[row,col] = 99
+            #adds the current pos to the visited set
+            visited.add(current_pos)
+            stack.append(current_pos)
+            #changes the position of the current pos to the next pos
+            current_pos = next_position
+            row,col = current_pos
+            valid_maze[row,col] = 1
+        else:
+            stack.pop()
+            if stack:
+                current_pos = stack[-1]
+    #stack is outputted in the order of the inner maze instead of the full maze
+    #print(stack)
     return valid_maze
 #endregion
 
-initalize_maze(maze)
+
